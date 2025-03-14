c2fd2d09cafdbe898fa7af25cc956f9810319b8c
Carlos Machado <user@example.com>
2025-03-14 17:20:36 +0100

    Working morpho market creation

---
+++ src/interfaces/IMorpho.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IMorpho {
+    /// @notice Emitted when a market is created.
+    /// @param id The id of the created market.
+    /// @param loanToken The address of the loan token of the created market.
+    /// @param collateralToken The address of the collateral token of the created market.
+    /// @param oracle The address of the oracle of the created market.
+    /// @param irm The address of the IRM of the created market.
+    /// @param lltv The LLTV of the created market.
+    event CreateMarket(
+        uint256 indexed id,
+        address indexed loanToken,
+        address indexed collateralToken,
+        address oracle,
+        address irm,
+        uint256 lltv
+    );
+
+    /// @notice Creates a market.
+    /// @param loanToken The address of the loan token of the market to create.
+    /// @param collateralToken The address of the collateral token of the market to create.
+    /// @param oracle The address of the oracle of the market to create.
+    /// @param irm The address of the IRM of the market to create.
+    /// @param lltv The LLTV of the market to create. Should be in units of 1e18. For example, a 90% LLTV would be
+    /// represented as 0.9e18.
+    /// @return id The id of the created market.
+    function createMarket(
+        address loanToken,
+        address collateralToken,
+        address oracle,
+        address irm,
+        uint256 lltv
+    ) external returns (uint256 id);
+} 
