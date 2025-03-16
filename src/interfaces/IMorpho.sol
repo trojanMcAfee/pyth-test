8f61917984b97d08771d3e2f8ddebf20a85a2091
Example User <user@example.com>
2025-03-16 21:31:51 +0100

    Reorged

--- src/interfaces/IMorpho.sol
+++ src/interfaces/IMorpho.sol
@@ -12,31 +12,6 @@ struct MarketParams {
 type Id is bytes32;
 
 interface IMorpho {
-   
-    // event CreateMarket(
-    //     uint256 indexed id,
-    //     address indexed loanToken,
-    //     address indexed collateralToken,
-    //     address oracle,
-    //     address irm,
-    //     uint256 lltv
-    // );
-
-    // /// @notice Creates a market.
-    // /// @param loanToken The address of the loan token of the market to create.
-    // /// @param collateralToken The address of the collateral token of the market to create.
-    // /// @param oracle The address of the oracle of the market to create.
-    // /// @param irm The address of the IRM of the market to create.
-    // /// @param lltv The LLTV of the market to create. Should be in units of 1e18. For example, a 90% LLTV would be
-    // /// represented as 0.9e18.
-    // /// @return id The id of the created market.
-    // function createMarket(
-    //     address loanToken,
-    //     address collateralToken,
-    //     address oracle,
-    //     address irm,
-    //     uint256 lltv
-    // ) external returns (uint256 id);
 
     event CreateMarket(Id indexed id, MarketParams marketParams);
 
